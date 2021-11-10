30e50f4bff28a861a5c6262b6cd35025b96d0287
Yurii <yuriikuzhil@gmail.com>
2021-11-10 21:52:14 +0200

    updated files

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,2 +1,30 @@
 const API_KEY = '19216489-5c3816338c51dfbca2dca2232';
 const BASE_URL = 'https://pixabay.com/api';
+
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchImages() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(({ hits }) => {
+        this.incrementPage();
+        return hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,22 +1,31 @@
 import imagesGalleryTpl from '../templates/images-gallery.hbs';
+import ApiService from './apiService';
 
 const refs = {
-  container: document.querySelector('.container'),
+  //   container: document.querySelector('.container'),
+  listgallery: document.querySelector('.gallery'),
   searchForm: document.querySelector('#search-form'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
-
+const apiService = new ApiService();
 refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
 function renderListGallery(images) {
-  refs.container.insertAdjacentHTML('beforeend', imagesGalleryTpl(images));
+  refs.listgallery.insertAdjacentHTML('beforeend', imagesGalleryTpl(images));
 }
 
 function onSearch(e) {
   e.preventDefault();
+  clearListGallery();
+  apiService.query = e.currentTarget.elements.query.value;
+  apiService.resetPage();
+  apiService.fetchImages().then(renderListGallery);
+}
 
-  fetch(
-    'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&key=19216489-5c3816338c51dfbca2dca2232',
-  )
-    .then(response => response.json())
-    .then(renderListGallery);
+function onLoadMore() {
+  apiService.fetchImages().then(renderListGallery);
+}
+function clearListGallery() {
+  refs.listgallery.innerHTML = '';
 }
